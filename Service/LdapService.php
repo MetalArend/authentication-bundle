f533619fe986f7c0fa35bb74573cb3d5938cb089
Bart Reunes <bart.reunes@kuleuven.be>
2016-08-09 10:23:09 +0200

    use symfony internal LDAP service

--- Service/LdapService.php
+++ Service/LdapService.php
@@ -2,107 +2,51 @@
 
 namespace Kuleuven\AuthenticationBundle\Service;
 
+use Symfony\Component\Ldap\Ldap;
+
 class LdapService
 {
     protected $rdn;
     protected $password;
     protected $base;
-    protected $domain;
-    protected $port;
-    protected $handle;
-    protected $bind;
 
     public function __construct(
+        Ldap $ldap,
         $rdn = '',
         $password = '',
-        $base = 'ou=people,dc=kuleuven,dc=be',
-        $domain = 'ldap.kuleuven.be',
-        $port = 389,
-        $ssl = false
+        $base = 'ou=people,dc=kuleuven,dc=be'
     )
     {
+        $this->ldap = $ldap;
         $this->rdn = $rdn;
         $this->password = $password;
         $this->base = $base;
-        if (false === strpos($domain, '://')) {
-            $domain = ($ssl ? 'ldaps://' : 'ldap://') . $domain;
-        }
-        $this->domain = $domain;
-        $this->port = $port;
-
-        if (!defined('LDAP_OPT_DIAGNOSTIC_MESSAGE')) {
-            define('LDAP_OPT_DIAGNOSTIC_MESSAGE', 0x0032);
-        }
-    }
-
-    public function __destruct()
-    {
-        $this->unbind();
-    }
-
-    protected function connect()
-    {
-        if (empty($this->handle)) {
-            $this->handle = \ldap_connect($this->domain, $this->port);
-            if (false === $this->handle) {
-                throw new \Exception('Could not connect to LDAP server: ' . $this->error());
-            }
-            \ldap_set_option($this->handle, LDAP_OPT_PROTOCOL_VERSION, 3);
-            \ldap_set_option($this->handle, LDAP_OPT_REFERRALS, 0);
-        }
-        return $this;
     }
 
     protected function bind()
     {
-        $this->connect();
-        if (empty($this->bind)) {
-            $this->bind = @\ldap_bind($this->handle, $this->rdn, $this->password);
-            if (false === $this->bind) {
-                throw new \Exception('Could not bind to LDAP server: ' . $this->error());
-            }
-        }
-        return $this;
+        $this->ldap->bind($this->rdn, $this->password);
     }
 
-    protected function unbind()
-    {
-        if (!empty($this->handle)) {
-            \ldap_unbind($this->handle);
-        }
-        return $this;
-    }
-
-    public function createFilter(array $data = [], $type = '&', $wrap = '%s')
+    public function createFilter(array $data = [], $type = '&', $format = '%s')
     {
         $string = '';
         foreach ($data as $key => $value) {
             if (is_array($value)) {
-                $string .= $this->createFilter($value, $key, $wrap);
+                $string .= $this->createFilter($value, $key, $format);
             } else {
-                $value = sprintf($wrap, $value);
+                $value = sprintf($format, $value);
                 $string .= "(${key}=${value})";
             }
         }
         return "(${type}${string})";
     }
 
-    public function search($filter, $attributes = ['uid', 'sn', 'givenname', 'mail', 'ou'], $limit = 100, $fuzzy = false)
+    public function search($filter, $attributes = [], $attrsOnly = false, $sizeLimit = 0, $timeLimit = 0)
     {
-        if (empty($filter)) {
-            return null;
-        }
-
-        // Bind
-        $this->bind();
-
-        // Add fuzzy
-        $wrap = $fuzzy;
-        if (is_bool($wrap)) {
-            $wrap = $fuzzy ? '*%s*' : '%s';
-        }
+        // Create filter
         if (is_array($filter)) {
-            $filter = $this->createFilter($filter, '&', $wrap);
+            $filter = $this->createFilter($filter, '&', '%s');
         }
 
         // Catch empty attributes
@@ -110,34 +54,27 @@ class LdapService
             $attributes = [];
         }
 
+        // Bind
+        $this->bind();
+
         // Search
-        $results = false;
-        if ($this->bind) {
-            $search = @\ldap_search($this->handle, $this->base, $filter, $attributes, 0, $limit); // add @ to suppress ldap warning
-            if (empty($search)) {
-                throw new \Exception('Could not search LDAP: ' . $this->error());
-            }
-            $results = \ldap_get_entries($this->handle, $search);
-        }
+        $results = $this->ldap->query($this->base, $filter, [
+            'attrsOnly' => $attrsOnly,
+            'filter'    => $attributes,
+//            'maxItems'  => $maxItems,
+            'sizeLimit' => $sizeLimit,
+            'timeout'   => $timeLimit,
+        ])->execute();
 
         // Return
         return $results;
     }
 
-    public function errno()
+    public function fuzzy($filter, $attributes = [], $attrsOnly = false, $sizeLimit = 0, $timeLimit = 0)
     {
-        $this->bind();
-
-        return \ldap_errno($this->handle);
-    }
-
-    public function error()
-    {
-        $this->bind();
-
-        if (\ldap_get_option($this->handle, LDAP_OPT_DIAGNOSTIC_MESSAGE, $extended_error)) {
-            return $extended_error;
+        if (is_array($filter)) {
+            $filter = $this->createFilter($filter, '&', '*%s*');
         }
-        return \ldap_error($this->handle);
+        $this->search($filter, $attributes, $attrsOnly, $sizeLimit, $timeLimit);
     }
 }
